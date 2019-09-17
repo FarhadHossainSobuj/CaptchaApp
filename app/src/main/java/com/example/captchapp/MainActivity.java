69305f74eb37ad5f8189ca0e7fc1dc887c6c0119
Farhad Hossain <32379259+FarhadHossainSobuj@users.noreply.github.com>
2019-09-17 13:29:57 +0600

    Random Image feature added

---
+++ app/src/main/java/com/example/captchapp/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.captchapp;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static final int PERMISSION_REQUEST_CODE=100;
+    Button read;
+
+    private ImageView img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img0 = findViewById(R.id.img0);
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.img2);
+        img3 = findViewById(R.id.img3);
+        img4 = findViewById(R.id.img4);
+        img5 = findViewById(R.id.img5);
+        img6 = findViewById(R.id.img6);
+        img7 = findViewById(R.id.img7);
+        img8 = findViewById(R.id.img8);
+        img9 = findViewById(R.id.img9);
+        img10 = findViewById(R.id.img10);
+        img11 = findViewById(R.id.img11);
+        img12 = findViewById(R.id.img12);
+        img13 = findViewById(R.id.img13);
+        img14 = findViewById(R.id.img14);
+        img15 = findViewById(R.id.img15);
+        List<ImageView> list = new ArrayList<>(15);
+        list.add(img0);
+        list.add(img1);
+        list.add(img2);
+        list.add(img3);
+        list.add(img4);
+        list.add(img5);
+        list.add(img6);
+        list.add(img7);
+        list.add(img8);
+        list.add(img9);
+        list.add(img10);
+        list.add(img11);
+        list.add(img12);
+        list.add(img13);
+        list.add(img14);
+        list.add(img15);
+
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i=0; i<=11; i++) {
+            list1.add(new Integer(i));
+        }
+        Collections.shuffle(list1);
+
+        if(isStoragePermissionGranted()){
+            for (int i = 0; i < 16; i++){
+                if(i < 12){
+                    list.get(i).setImageBitmap(BitmapFactory.decodeFile("/storage/emulated/0/img/a0"+list1.get(i)+".png"));
+                } else {
+                    list.get(i).setImageBitmap(BitmapFactory.decodeFile("/storage/emulated/0/img/b0"+(i-11)+".png"));
+                }
+            }
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v("TAG","Permission is granted");
+                return true;
+            } else {
+
+                Log.v("TAG","Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("TAG","Permission is granted");
+            return true;
+        }
+    }
+}
