3f7a49e5252b75ab91faed314164c5e080b589bb
Farhad Hossain <32379259+FarhadHossainSobuj@users.noreply.github.com>
2019-09-19 01:17:53 +0600

    captcha accuracy calculation

--- app/src/main/java/com/example/captchapp/MainActivity.java
+++ app/src/main/java/com/example/captchapp/MainActivity.java
@@ -1,39 +1,48 @@
 package com.example.captchapp;
 import android.Manifest;
-import android.app.Activity;
+import android.content.ClipData;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
-import java.io.File;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, View.OnDragListener {
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
     private static final int PERMISSION_REQUEST_CODE=100;
-    Button read;
+
+    private int dragCounter, success;
 
     private ImageView img0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int imgIndex = Integer.parseInt(getIntent().getStringExtra("testImageNo"));
+        int totalTestImage = 6;
 
         img0 = findViewById(R.id.img0);
         img1 = findViewById(R.id.img1);
@@ -76,11 +85,13 @@ public class MainActivity extends AppCompatActivity {
         for (int i=0; i<=15; i++) {
             list1.add(new Integer(i));
         }
+
+        int rand = new Random().nextInt(5);
         for (int i=0; i<=15; i++) {
             if(i < 12){
-                bitmaps.add(BitmapFactory.decodeFile("/storage/emulated/0/img/a0"+(i+1)+".png"));
+                bitmaps.add(BitmapFactory.decodeFile("/storage/emulated/0/img/"+imgIndex+"/"+(i+1)+".png"));
             } else {
-                bitmaps.add(BitmapFactory.decodeFile("/storage/emulated/0/img/b0"+(i-11)+".png"));
+                bitmaps.add(BitmapFactory.decodeFile("/storage/emulated/0/img/"+rand+"/"+(i-3)+".png"));
             }
         }
         Collections.shuffle(list1);
@@ -93,15 +104,25 @@ public class MainActivity extends AppCompatActivity {
                 list.get(i).setTag(""+tagList.get(i));
             }
         }
-        img0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, ""+ img0.getTag(), Toast.LENGTH_SHORT).show();
-            }
-        });
+
+        for(int i = 0; i <= 15; i++){
+            list.get(i).setOnTouchListener(this);
+        }
+        findViewById(R.id.inputLayout1).setOnDragListener(this);
+        findViewById(R.id.inputLayout2).setOnDragListener(this);
+        findViewById(R.id.inputLayout3).setOnDragListener(this);
+        findViewById(R.id.inputLayout4).setOnDragListener(this);
+        findViewById(R.id.inputLayout5).setOnDragListener(this);
+        findViewById(R.id.inputLayout6).setOnDragListener(this);
+        findViewById(R.id.inputLayout7).setOnDragListener(this);
+        findViewById(R.id.inputLayout8).setOnDragListener(this);
+        findViewById(R.id.inputLayout9).setOnDragListener(this);
+        findViewById(R.id.inputLayout10).setOnDragListener(this);
+        findViewById(R.id.inputLayout11).setOnDragListener(this);
+        findViewById(R.id.inputLayout12).setOnDragListener(this);
 
     }
-    
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
@@ -140,5 +161,81 @@ public class MainActivity extends AppCompatActivity {
             Log.v("TAG","Permission is granted");
             return true;
         }
+    }
+
+    @Override
+    public boolean onDrag(View v, DragEvent event) {
+        switch (event.getAction()) {
+            // signal for the start of a drag and drop operation
+            case DragEvent.ACTION_DRAG_STARTED:
+                // do nothing
+                break;
+
+            // the drag point has entered the bounding box of the View
+            case DragEvent.ACTION_DRAG_ENTERED:
+                v.setBackgroundColor(0xFFFFF6F9);
+                break;
+
+            // the user has moved the drag shadow outside the bounding box of the View
+            case DragEvent.ACTION_DRAG_EXITED:
+                v.setBackgroundColor(v.getId() == R.id.inputLayout1 ? 0xFFE8E6E7 : 0xFFB1BEC4);
+                break;
+
+            // the drag and drop operation has concluded
+            case DragEvent.ACTION_DRAG_ENDED:
+                v.setBackgroundColor(v.getId() == R.id.inputLayout1 ? 0xFFE8E6E7 : 0xFFB1BEC4);
+                break;
+
+            //drag shadow has been released,the drag point is within the bounding box of the View
+            case DragEvent.ACTION_DROP:
+                String id = "";
+                View view = (View) event.getLocalState();
+                // we want to make sure it is dropped only to left and right parent view
+                if (v.getId() == R.id.inputLayout1 || v.getId() == R.id.inputLayout2 || v.getId() == R.id.inputLayout3
+                        || v.getId() == R.id.inputLayout4 || v.getId() == R.id.inputLayout5 || v.getId() == R.id.inputLayout6
+                        || v.getId() == R.id.inputLayout7 || v.getId() == R.id.inputLayout8 || v.getId() == R.id.inputLayout9
+                        || v.getId() == R.id.inputLayout10 || v.getId() == R.id.inputLayout11 || v.getId() == R.id.inputLayout12) {
+                    ViewGroup source = (ViewGroup) view.getParent();
+                    source.removeView(view);
+                    LinearLayout target = (LinearLayout) v;
+                    target.addView(view);
+                    id = target.getTag().toString();
+                }
+                // make view visible as we set visibility to invisible while starting drag
+                //Toast.makeText(this, "" + view.getTag().toString(), Toast.LENGTH_SHORT).show();
+                dragCounter = dragCounter + 1;
+                if(Integer.parseInt(id) == Integer.parseInt(view.getTag().toString())){
+                    success = success + 1;
+                }
+                view.setVisibility(View.VISIBLE);
+                if(dragCounter >= 12){
+                    Intent intent = new Intent(this, Result.class);
+                    intent.putExtra("result", "" + success);
+                    startActivity(intent);
+                }
+                //Toast.makeText(this, "Dragged: "+dragCounter+" Success: " + success, Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+
+            ClipData data = ClipData.newPlainText("id", view.getResources().getResourceEntryName(view.getId()));
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                view.startDragAndDrop(data, shadowBuilder, view, 0);
+            } else {
+                view.startDrag(data, shadowBuilder, view, 0);
+            }
+
+            view.setVisibility(View.INVISIBLE);
+            return true;
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/example/captchapp/TestImage.java
@@ -0,0 +1,67 @@
+package com.example.captchapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestImage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_test_image);
+
+        int totalTestImage = 6;
+
+        final ArrayList<Integer> list1 = new ArrayList<Integer>(totalTestImage);
+        ArrayList<Bitmap> bitmaps = new ArrayList<>(totalTestImage);
+        ArrayList<Integer> tagList = new ArrayList<Integer>(totalTestImage);
+
+        for (int i=0; i<=totalTestImage; i++) {
+            list1.add(new Integer(i));
+        }
+        for (int i=0; i<=totalTestImage; i++) {
+            bitmaps.add(BitmapFactory.decodeFile("/storage/emulated/0/img/"+i+"/"+"0.png"));
+        }
+        Collections.shuffle(list1);
+        ImageView iv = (ImageView)findViewById(R.id.imageView);
+        iv.setImageBitmap(bitmaps.get(list1.get(0)));
+
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent openStartingPoint = new Intent(TestImage.this,
+                            MainActivity.class);
+                    openStartingPoint.putExtra("testImageNo", "" + list1.get(0));
+                    startActivity(openStartingPoint);
+                }
+
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
